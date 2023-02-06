5b006409606b23857719712b5a56853a39288e3e
Dean Draper <deandraper82@gmail.com>
2023-02-06 15:05:09 -0600

    Reworked Footer to dynamically change sections

--- components/composite/Footer.jsx
+++ components/composite/Footer.jsx
@@ -13,71 +13,42 @@ export default function Footer(props) {
   });
 
   return (
-    <div className='container-fluid px-5 vw-100 bg-brand mt-7'>
+    <div className='container-fluid px-5 vw-100 bg-brand bg-gradient mt-7'>
       <div className='container'>
-        <footer className='py-5'>
-          <div className='d-flex w-100 flex-row justify-space-between'>
-            {/* Account */}
-            <div className='col-6 col-md-3 mb-3 border-top border-2 pt-2'>
-              <h5 className='text-uppercase text-light'>Account</h5>
-              <ul className='nav flex-column'>
-                {footerLinks.account.map((item, index) => (
-                  <li className='nav-item mb-2' key={index}>
-                    <a href={item.url} className='nav-link p-0 text-light'>
-                      {item.displayText}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className='col-md-1'></div>
-
-            {/* Explore */}
-            <div className='col-6 col-md-3 mb-3 border-top border-2 pt-2'>
-              <h5 className='text-uppercase text-light'>Explore Arkane</h5>
-              <ul className='nav flex-column'>
-                {footerLinks.explore.map((item, index) => (
-                  <li className='nav-item mb-2' key={index}>
-                    <a href={item.url} className='nav-link p-0 text-light'>
-                      {item.displayText}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className='col-md-1'></div>
-
-            {/* Contact */}
-            <div className='col-6 col-md-3 mb-3 border-top border-2 pt-2'>
-              <h5 className='text-uppercase text-light'>Get In Touch</h5>
-              <ul className='nav flex-column'>
-                {footerLinks.contact.map((item, index) => (
-                  <li className='nav-item mb-2' key={index}>
-                    <a href={item.url} className='nav-link p-0 text-light'>
-                      {item.displayText}
-                    </a>
-                  </li>
-                ))}
-                <ul className='list-unstyled d-flex mt-1'>
-                  <li>
-                    <BsLinkedin className='nav-link p-0 text-light' />
-                  </li>
-                  <li className='ms-3'>
-                    <BsFacebook className='nav-link p-0 text-light' />
-                  </li>
-                  <li className='ms-3'>
-                    <BsTwitter className='nav-link p-0 text-light' />
-                  </li>
-                  <li className='ms-3'>
-                    <FiMail className='nav-link p-0 text-light' />
-                  </li>
+        <footer className='py-5 w-100'>
+          <div className='d-flex w-100 flex-row justify-content-between'>
+            {Object.keys(footerLinks).map((key, index) => (
+              <div className='border-top border-2 pt-2 w-100 me-5' key={index}>
+                <h5 className='text-uppercase text-light'>{key}</h5>
+                <ul className='nav flex-column'>
+                  {footerLinks[key].map((item, index) => (
+                    <li className='nav-item mb-2' key={index}>
+                      <a href={item.url} className='nav-link p-0 text-light'>
+                        {item.displayText}
+                      </a>
+                    </li>
+                  ))}
                 </ul>
-              </ul>
-            </div>
+              </div>
+            ))}
           </div>
 
           <div className='d-flex flex-column flex-sm-row justify-content-between pt-4 mt-4 border-top text-light'>
             <p>© 2022 Arkane Digital, LLC. All rights reserved.</p>
+            <ul className='list-unstyled d-flex mt-1'>
+              <li>
+                <BsLinkedin className='nav-link p-0 text-light' />
+              </li>
+              <li className='ms-3'>
+                <BsFacebook className='nav-link p-0 text-light' />
+              </li>
+              <li className='ms-3'>
+                <BsTwitter className='nav-link p-0 text-light' />
+              </li>
+              <li className='ms-3'>
+                <FiMail className='nav-link p-0 text-light' />
+              </li>
+            </ul>
           </div>
         </footer>
       </div>
