5ff1f8171342e4ab647fa5c2844760f234b7841e
Dean Draper <deandraper82@gmail.com>
2023-05-12 12:29:21 -0500

    Working build, several more components

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,11 +1,18 @@
+import React from "react";
 import Footer from "@/components/layout/Footer";
 import NavBar from "@/components/layout/NavBar";
+import { FooterLinkP } from "@/services/types";
 // TODO: Footer links should be converted back to props.links once everything is working
+
+interface LayoutProps {
+  children: React.ReactNode;
+  links: FooterLinkP[];
+}
 
 /**
  * Layout is a top level wrapper that will wrap every page in `/pages`.
  */
-export default function Layout(props) {
+export default function Layout(props: LayoutProps): React.ReactElement {
   return (
     <div className='min-h-screen flex flex-col justify-between'>
       <NavBar />
