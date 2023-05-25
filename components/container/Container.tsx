af39be1e797f16f4139a20ac7c68685fcbc32fbd
Dean Draper <deandraper82@gmail.com>
2023-05-25 14:09:16 -0500

    Background image fade-in added

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -1,21 +1,98 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 interface ContainerProps {
   className?: string;
   children: React.ReactNode;
+  bgImageUrl?: string;
 }
 
-/**
- * This component is meant to be a top level wrapper component in the main content area of a page.
- * It is a container for a component in the components\composite folder.
- * @returns {React.ReactElement} A wrapper component for a layout element.
- */
-export default function Container({ children, className="" }: ContainerProps): React.ReactElement {
+export default function Container({ children, className = "", bgImageUrl = "" }: ContainerProps): React.ReactElement {
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (bgImageUrl && imageRef.current) {
+      gsap.fromTo(
+        imageRef.current,
+        { opacity: 0 },
+        { opacity: 1, duration: 3, ease: "power2.inOut" }
+      );
+    }
+  }, [bgImageUrl]);
+
+  const bgImage = {
+    backgroundImage: bgImageUrl ? `url(${bgImageUrl})` : "",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  };
+
   return (
-    <div className={`flex items-center ${className}`}>
-      <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-        <div className='flex flex-col md:flex-row items-center'>{children}</div>
-      </div>
-    </div>
+    <>
+      {bgImageUrl && (
+        <div ref={imageRef} style={bgImage}>
+          <div className={`flex items-center ${className}`}>
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex flex-col md:flex-row items-center">{children}</div>
+            </div>
+          </div>
+        </div>
+      )}
+      {!bgImageUrl && (
+        <div className={`flex items-center ${className}`}>
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-col md:flex-row items-center">{children}</div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
+
+
+// import React, { useLayoutEffect } from "react";
+
+// import { gsap } from "gsap";
+
+// interface ContainerProps {
+//   className?: string;
+//   children: React.ReactNode;
+//   bgImageUrl?: string;
+// }
+
+// /**
+//  * This component is meant to be a top level wrapper component in the main content area of a page.
+//  * It is a container for a component in the components\composite folder.
+//  * @returns {React.ReactElement} A wrapper component for a layout element.
+//  */
+// export default function Container({ children, className = "", bgImageUrl = "" }: ContainerProps): React.ReactElement {
+//   const imageRef = React.useRef();
+
+//   const bgImage = {
+//     backgroundImage: bgImageUrl ? `url(${bgImageUrl})` : "",
+//     backgroundRepeat: "no-repeat",
+//     backgroundSize: "cover",
+//     backgroundPosition: "center center",
+//   }
+
+//   useLayoutEffect(() => {
+//     gsap.from(imageRef, {},);
+//   }, []);
+
+//   return (
+//     <>
+//       {bgImageUrl && (<div ref={imageRef} id="imageFadeIn" style={bgImage}>
+//         <div className={`flex items-center ${className}`}>
+//           <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+//             <div className='flex flex-col md:flex-row items-center'>{children}</div>
+//           </div>
+//         </div>
+//       </div>)}
+//       {!bgImageUrl && (<div className={`flex items-center ${className}`}>
+//         <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+//           <div className='flex flex-col md:flex-row items-center'>{children}</div>
+//         </div>
+//       </div>)}
+//     </>
+//   );
+// }
--- pages/gsap.tsx
+++ pages/gsap.tsx
@@ -33,12 +33,12 @@ export default function GSAP({ links }: GSAPProps): React.ReactElement {
 
       const tl = gsap.timeline({ defaults: { duration: 2, ease: "none" } });
 
-      tl.to("#scramble", { duration: 1, scrambleText: { text: "At Arkane Digital, we believe in a \"Strategy First\" approach. Specializing in headless Content Management Systems (CMS), we provide your business with the tools necessary to prosper in today's digital age.", chars: "upperAndLowerCase" } })
-        .to("#charsCustom", { duration: 1, scrambleText: { text: "Headless CMS represents a breakthrough in content management. It separates back-end content creation and storage from front-end presentation, enabling multi-platform content distribution—from websites and mobile apps to IoT devices and beyond—with ease.", chars: "upperAndLowerCase" } })
+      tl.to("#scramble", { duration: 1, scrambleText: { text: "At Arkane Digital, we believe in a \"Strategy First\" approach. Specializing in headless Content Management Systems (CMS), we provide your business with the tools necessary to prosper in today's digital age.", chars: "upperAndLowerCase", newClass: "opacity-100" } })
+        .to("#charsCustom", { duration: 1, scrambleText: { text: "Headless CMS represents a breakthrough in content management. It separates back-end content creation and storage from front-end presentation, enabling multi-platform content distribution—from websites and mobile apps to IoT devices and beyond—with ease.", chars: "upperAndLowerCase", newClass: "opacity-100" } })
         .to("#charsNumbers", { duration: 0.5, scrambleText: { text: "However, the power of headless CMS comes with its complexities. This is where Arkane Digital shines. Our team of adept software consultants is committed to unlocking this potent technology's potential for your business. We begin with a comprehensive understanding of your business and its unique needs. Then, we devise a strategic plan, customized for your business, to integrate headless CMS into your digital infrastructure smoothly and efficiently.", chars: "upperAndLowerCase" } })
         .to("#charsUppercase", { duration: 0.5, scrambleText: { text: "Whether you're just starting with headless CMS or looking to optimize an existing system, Arkane Digital stands ready to guide you. We possess the knowledge and experience to navigate this technology's intricacies, ensuring you maximize its benefits while minimizing any challenges.", chars: "upperAndLowerCase" } })
         .to("#charsLowercase", { duration: 0.5, scrambleText: { text: "At Arkane Digital, you're not just engaging a software consultancy—you're gaining a dedicated partner in your digital journey. Our commitment is to put your business strategy at the forefront of every decision. With a \"Strategy First\" approach, we help you take control of your digital landscape, making headless CMS a cornerstone of your digital strategy.", chars: "upperAndLowerCase" } })
-        .to("#newClass", { duration: 0.5, scrambleText: { text: "Contact us today to get started.", chars: "upperAndLowerCase", newClass: "text-primary underline text-2xl" } })
+        .to("#newClass", { duration: 0.5, scrambleText: { text: "Contact us today to get started.", chars: "upperAndLowerCase", newClass: "text-primary underline text-2xl opacity-100" } })
 
       // GSDevTools.create({ animation: tl, minimal: true });
     }, box)
@@ -48,16 +48,18 @@ export default function GSAP({ links }: GSAPProps): React.ReactElement {
 
   return (
     <Layout links={links}>
-      <ContainerLight>
+      <Container
+        bgImageUrl='https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=710&q=80'
+      >
 
         <div ref={box} className='mx-auto'>
           <div className='mx-auto pt-12 text-center'>
-            <span className='text-primary text-8xl font-bold inline-block left'>Strategy </span>
-            <span className='text-blue-700 text-8xl font-bold inline-block right'> First</span>
+            <span className='text-primary text-8xl font-extrabold inline-block left'>Strategy&nbsp;</span>
+            <span className='text-blue-700 text-8xl font-extrabold inline-block right'>First</span>
           </div>
           <div className='mx-auto py-12 demo'>
-            <div id="textblock" className='max-w-5xl text-xl p-12 bg-brand-light shadow-xl rounded-lg text-brand-dark'>
-              <div id="scramble"></div>
+            <div id="textblock" className='max-w-5xl text-2xl p-12 bg-brand-light shadow-xl rounded-lg text-brand-dark bg-opacity-90'>
+              <div id="scramble">At Arkane Digital, we believe in a strategy first approach. Specializing in headless Content Management Systems (CMS), we provide your business with the tools necessary to prosper in today`&apos`s digital age.</div>
               <br />
               <div id="charsCustom"></div>
               <br />
@@ -73,7 +75,7 @@ export default function GSAP({ links }: GSAPProps): React.ReactElement {
             </div>
           </div>
         </div>
-      </ContainerLight>
+      </Container>
     </Layout>
   )
 }
